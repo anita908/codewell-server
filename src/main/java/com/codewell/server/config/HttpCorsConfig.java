2dc45f5ba65a264e7ce24868e5d64155221689f1
sunnydtakoyaki <sunny.yang@databased.com>
2021-03-04 18:11:49 -0700

    fix cors issue

--- src/main/java/com/codewell/server/config/HttpCorsConfig.java
+++ src/main/java/com/codewell/server/config/HttpCorsConfig.java
@@ -16,7 +16,6 @@ public class HttpCorsConfig
     {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.addAllowedOrigin(CorsConfiguration.ALL);
-        corsConfiguration.setAllowCredentials(true);
         corsConfiguration.setAllowedHeaders(Arrays.asList("origin", "content-type", "accept", "authorization"));
         corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
